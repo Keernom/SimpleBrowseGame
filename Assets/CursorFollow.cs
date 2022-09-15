98f97ceeeb92581fd9a0b6d44e0ea3e04750bf16
Keernom <keernom.gdev@gmail.com>
2022-09-16 00:22:17 +0300

    Player controller scratch added.

---
+++ Assets/CursorFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorFollow : MonoBehaviour
+{
+    [SerializeField] LayerMask _ground;
+
+    private void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 50, _ground))
+        {
+            Vector3 pos = hit.point;
+            pos.y = 0;
+            transform.SetPositionAndRotation(pos, Quaternion.identity);
+        }
+    }
+}
